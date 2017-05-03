7868a45352b4729348326291ccf3b9e5215147fd
Eisbehr <git@eisbehr.de>
2017-05-03 10:41:30 +0200

    added gulp jshint validation task

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,7 @@
 let gulp = require("gulp");
 let jshint = require("gulp-jshint");
 
-// noinspection JSUnresolvedFunction
 gulp.task("validate", () => {
-    // noinspection JSUnresolvedFunction
     return gulp.src(["examples/**/*.js", "lib/**/*.js", "test/**/*.js", "index.js"])
                .pipe(jshint())
                .pipe(jshint.reporter("jshint-stylish"));
